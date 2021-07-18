2dc9f91a9e4e5f0f1972b9e84fc7746ca8da7054
Khoa <khoa150899@gmail.com>
2021-07-18 02:33:47 -0400

    Initial Commit: - Created Gradle project and added javafx library. - Created main App.java and controllers for main, import, export scenes. - Created .fxml files for each scene. - Designed layout for main page with listview and button for import/export/help/sort/edit.

---
+++ src/main/java/ucf/assignments/App.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Khoa Phan
+ */
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    private static Stage stage; //Stage variable will be created to change scene later
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        stage = primaryStage; //current stage will be duplicated to change scene later
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/mainPage.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Main Page");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //method will change scene using specified fxml file name
+    public void changeScene(String fxml) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(fxml));
+            stage.getScene().setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ucf/assignments/mainPageController.java
@@ -0,0 +1,51 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class mainPageController {
+
+    @FXML
+    private ListView<String> itemList;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField serialField;
+
+    @FXML
+    private TextField valueField;
+
+    @FXML
+    private ChoiceBox<?> sortChoiceBox;
+
+    @FXML
+    void exportFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getHelp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void importFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveItem(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectItem(ActionEvent event) {
+
+    }
+
+}
